8f6f422989963cc1bdad6420e16277c126332ff4
mfcicek <fc.fatihcicek@gmail.com>
2021-10-12 19:45:26 +0300

    Linkler Güncellendi

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express');
-
+const ejs = require('ejs');
 const app = express();
+
+//Template Engine
+app.set('view engine', 'ejs');
+
+//Middlewares
+app.use(express.static('public'));
 
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.status(200).send('sample');
+  res.status(200).render('index', {
+    page_name: 'index',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.status(200).render('about', {
+    page_name: 'about',
+  });
 });
 
 app.listen(port, () => {
