137eea6ba93f16d1fedc92368eb8042f04422314
mfcicek <fc.fatihcicek@gmail.com>
2021-10-14 20:47:41 +0300

    course-öğrenci ilişkisi kuruldu, search alanı aktifleştirildi

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,5 +1,6 @@
 const Course = require('../models/Course');
 const Category = require('../models/Category');
+const User = require('../models/User');
 
 exports.createCourse = async (req, res) => {
   try {
@@ -22,15 +23,34 @@ exports.createCourse = async (req, res) => {
 exports.getAllCourses = async (req, res) => {
   try {
     const categorySlug = req.query.categories;
+    const query = req.query.search;
     const category = await Category.findOne({ slug: categorySlug });
 
     let filter = {};
+
+    if (query) filter = { name: query };
 
     if (categorySlug) {
       filter = { category: category._id };
     }
 
-    const courses = await Course.find(filter).sort('-createdAt');
+    if (query) {
+      filter = { name: query };
+    }
+
+    if (!query && !categorySlug) {
+      filter.name = '';
+      filter.category = null;
+    }
+
+    const courses = await Course.find({
+      $or: [
+        { name: { $regex: '.*' + filter.name + '.*', $options: 'i' } },
+        { category: filter.category },
+      ],
+    })
+      .sort('-createdAt')
+      .populate('user');
     const categories = await Category.find();
 
     res.status(200).render('courses', {
@@ -52,14 +72,56 @@ exports.getAllCourses = async (req, res) => {
 
 exports.getCourse = async (req, res) => {
   try {
+    const user = await User.findById(req.session.userID);
     const course = await Course.findOne({ slug: req.params.slug }).populate(
       'user'
     );
+    const categories = await Category.find();
 
     res.status(200).render('course', {
       course,
       page_name: 'courses',
+      user,
+      categories,
     });
+    /*res.status(200).json({
+      status: 'success',
+      courses,
+    }); */
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+
+exports.enrollCourse = async (req, res) => {
+  try {
+    const user = await User.findById(req.session.userID);
+    await user.courses.push({ _id: req.body.course_id });
+    await user.save();
+
+    res.status(200).redirect('/users/dashboard');
+    /*res.status(200).json({
+      status: 'success',
+      courses,
+    }); */
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+
+exports.releaseCourse = async (req, res) => {
+  try {
+    const user = await User.findById(req.session.userID);
+    await user.courses.pull({ _id: req.body.course_id });
+    await user.save();
+
+    res.status(200).redirect('/users/dashboard');
     /*res.status(200).json({
       status: 'success',
       courses,
