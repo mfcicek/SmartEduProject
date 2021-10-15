c307ded5cd803c17e7615b3e39235708aa8b0024
mfcicek <fc.fatihcicek@gmail.com>
2021-10-15 07:12:25 +0300

    delete course

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -133,3 +133,16 @@ exports.releaseCourse = async (req, res) => {
     });
   }
 };
+
+exports.deleteCourse = async (req, res) => {
+  try {
+    const course = await Course.findOneAndRemove({ slug: req.params.slug });
+    req.flash('success', `${course.name} has been removed successfully`);
+    res.status(200).redirect('/users/dashboard');
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
